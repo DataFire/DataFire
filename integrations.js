1efd4de780843a39ad3ff39e596ed90ae62c0446
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-07 19:02:14 +0000

    more descriptions work

--- integrations.js
+++ integrations.js
@@ -1,5 +1,8 @@
 let fs = require('fs');
 let request = require('request');
+let chalk = require('chalk');
+let columnify = require('columnify');
+
 let datafire = require('./index');
 
 const APIS_GURU_URL = "https://api.apis.guru/v2/list.json";
@@ -56,31 +59,80 @@ integrations.list = (args, cb) => {
   }
 }
 
-let logMax = (str) => {
+let logDescription = (str) => {
   if (!str) return;
   if (str.length > MAX_DESCRIPTION_LENGTH) {
     str = str.substring(0, MAX_DESCRIPTION_LENGTH) + '...';
   }
-  console.log(str);
+  console.log(chalk.gray(str));
 }
 
 integrations.describe = (args) => {
   let integration = new datafire.Integration(args.name);
   integration.initialize(err => {
     let spec = integration.spec;
-    let url = spec.schemes[0] + '://' + spec.host + spec.basePath;
     console.log('\n');
-    console.log(url);
-    logMax(spec.info.description);
-    console.log('\n');
+    if (!args.operation) {
+      let url = spec.schemes[0] + '://' + spec.host + spec.basePath;
+      console.log(chalk.blue(url));
+      logDescription(spec.info.description);
+      console.log('\n');
+    }
     Object.keys(spec.paths).forEach(path => {
       Object.keys(spec.paths[path]).forEach(method => {
         let op = spec.paths[path][method];
-        if (op.operationId) console.log(op.operationId);
-        console.log(method.toUpperCase() + '\t' + path);
-        logMax(op.description);
+        if (args.operation) {
+          let fakeOpId = new RegExp(method + '\\s+' + path, 'i');
+          if (args.operation !== op.operationId && !args.operation.match(fakeOpId)) {
+            return;
+          }
+        }
+        integrations.describeOperation(method, path, op, args.operation ? true : false);
         console.log();
       });
     });
   });
+}
+
+let chalkMethod = (method) => {
+  method = method.toUpperCase();
+  if (method === 'GET') return chalk.green(method);
+  if (method === 'PUT' || method === 'POST' || method === 'PATCH') return chalk.yellow(method);
+  if (method === 'DELETE') return chalk.red(method);
+  return method;
+}
+
+let chalkType = (type) => {
+  type = type || 'string';
+  if (type === 'string') return chalk.green(type);
+  if (type === 'integer' || type === 'number') return chalk.blue(type);
+  if (type === 'boolean') return chalk.yellow(type);
+  if (type === 'array' || type === 'object') return chalk.magenta(type);
+  return type;
+}
+
+integrations.describeOperation = (method, path, op, verbose) => {
+  console.log(chalkMethod(method) + '\t' + path);
+  if (op.operationId) console.log(op.operationId);
+  if (!verbose) {
+    logDescription(op.description);
+    return;
+  }
+  console.log(chalk.gray(op.description));
+  console.log()
+  let paramDescriptions = op.parameters.map(p => {
+    let ret = {parameter: p.name};
+    ret.type = chalkType(p.in === 'body' ? 'object': p.type);
+    if (p.description) {
+      ret.description = chalk.gray(p.description);
+    }
+    return ret;
+  })
+  console.log(columnify(paramDescriptions, {
+    config: {
+      description: {
+        maxWidth: 80
+      }
+    }
+  }));
 }
