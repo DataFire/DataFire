306c98edd6250c312a007dc5a70886ebb1fa034d
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-08 21:13:38 +0000

    add local specs to list

--- commands/list.js
+++ commands/list.js
@@ -6,17 +6,21 @@ const logger = require('../lib/logger');
 
 const FILE_SUFFIX = '.openapi.json';
 const APIS_GURU_URL = "https://api.apis.guru/v2/list.json";
+const LOCAL_SPECS_DIR = __dirname + '/../integration_files';
+const LOCAL_SPECS = fs.readdirSync(LOCAL_SPECS_DIR).map(f => f.substring(0, f.length - FILE_SUFFIX.length));
 
 module.exports = (args) => {
   if (args.all) {
     request.get(APIS_GURU_URL, {json: true}, (err, resp, body) => {
       if (err) throw err;
-      let keys = Object.keys(body);
+      let keys = Object.keys(body).concat(LOCAL_SPECS).sort();
       keys.forEach(k => {
-        let api = body[k];
-        api = api.versions[api.preferred];
         logger.log(chalk.magenta(k));
-        logger.logDescription(api.info.description);
+        let api = body[k];
+        if (api) {
+          api = api.versions[api.preferred];
+          logger.logDescription(api.info.description);
+        }
         logger.log();
       });
     });
