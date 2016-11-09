d2dfdd3d4aff240ae68668bbe87fc60df00b4474
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-09 14:24:33 -0500

    add basic auth to test server

--- test/util/server.js
+++ test/util/server.js
@@ -4,9 +4,20 @@ app.get('/succeed', (req, res) => {
   res.json("OK");
 })
 
-app.get('/secret', (req, res) => {
-  if (req.query.api_secret !== 'secretsauce') {
-    return res.status(401).send("Incorrect api_secret");
+let authorize = (req, res, next) => {
+  if (req.headers.authorization) {
+    let key = req.headers.authorization.split(' ')[1];
+    let str = new Buffer(key, 'base64').toString();
+    let creds = str.split(':');
+    if (creds[0] === 'user1' && creds[1] === 'secretsauce') {
+      return next();
+    }
+  } else if (req.query.api_secret && req.query.api_secret === 'secretsauce') {
+    return next();
   }
+  res.status(401).send("Unauthorized");
+}
+
+app.get('/secret', authorize, (req, res) => {
   res.json("OK");
 })
