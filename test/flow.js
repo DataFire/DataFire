00aa062d3a9807d211012f7674b127e117df88d3
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-09 13:45:35 -0500

    basic test working

--- test/flow.js
+++ test/flow.js
@@ -1,11 +1,22 @@
 let expect = require('chai').expect
 
 let datafire = require('../index');
+datafire.integrationsDirectory = __dirname + '/integrations';
 
 describe('Flows', () => {
-  it('should run', () => {
+  it('should run', (done) => {
     let flow = new datafire.Flow('test', 'test_flow');
-    expect(flow).to.not.be.null;
+    let hn = new datafire.Integration('hacker_news');
+    flow.step('user',
+              hn.getUser(),
+              {username: 'sama'})
+    flow.step('result',
+              (data) => {
+                expect(data.user).to.not.be.null;
+                expect(data.user.id).to.equal('sama');
+                done();
+              })
+    flow.execute();
   })
 })
 
