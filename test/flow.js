e6458f8492aa7817fe848bdabeb34e0a9c32257f
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-09 14:03:53 -0500

    add auth endpoint

--- test/flow.js
+++ test/flow.js
@@ -7,20 +7,39 @@ let datafire = require('../index');
 datafire.integrationsDirectory = __dirname + '/integrations';
 
 describe('Flows', () => {
+  let flow = integration = null;
+
   before(done => {
     require('./util/server').listen(3333, done);
+  });
+
+  beforeEach(() => {
+    flow = new datafire.Flow('test', 'test_flow');
+    integration = new datafire.Integration('test');
   })
 
   it('should run', (done) => {
-    let flow = new datafire.Flow('test', 'test_flow');
-    let integ = new datafire.Integration('test');
-    flow.step('succeed', integ.get('/succeed'));
+    flow.step('succeed', integration.get('/succeed'));
     flow.step('result',
               (data) => {
                 expect(data.succeed).to.not.be.null;
                 expect(data.succeed).to.equal('OK');
                 done();
               })
+    flow.execute();
+  })
+
+  it('should not use auth by default', (done) => {
+    flow.step('fail', integration.get('/secret'))
+    flow.catch((err, data) => {
+          expect(err.statusCode).to.equal(401);
+          data.error = true;
+        })
+    flow.step('result',
+        (data) => {
+          expect(data.error).to.equal(true);
+          done();
+        })
     flow.execute();
   })
 })
--- test/util/server.js
+++ test/util/server.js
@@ -4,3 +4,9 @@ app.get('/succeed', (req, res) => {
   res.json("OK");
 })
 
+app.get('/secret', (req, res) => {
+  if (req.query.api_secret !== 'secretsauce') {
+    return res.status(401).send("Incorrect api_secret");
+  }
+  res.json("OK");
+})
