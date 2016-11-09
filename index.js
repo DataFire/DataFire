ce6a803a7c5e1361bd59c4530c6e37f5555e7bcb
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-09 13:28:48 -0500

    more command line help

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ datafire.Flow = require('./lib/flow');
 
 let COMMANDS = [{
   name: 'list',
-  description: "List integrations",
+  description: "List integrations in the current project, or all available integrations if -a is used",
+  examples: ["datafire list", "datafire list -a"],
   runner: require('./commands/list'),
   options: [{
     name: 'all',
@@ -19,28 +20,62 @@ let COMMANDS = [{
 }, {
   name: 'integrate <integration>',
   description: "Add an integration to the current project",
+  examples: ["datafire integrate hacker_news"],
   runner: require('./commands/integrate'),
   options: [{
     name: 'url',
     alias: 'u',
     description: "The URL of an Open API JSON specification",
+  }, {
+    name: 'as',
+    description: "An alias to use for the integration in this project",
+  }]
+}, {
+  name: 'describe <integration>',
+  description: "Show details for an integration or operation",
+  examples: [
+    "datafire describe hacker_news",
+    "datafire describe hacker_news -o getUser",
+  ],
+  runner: require('./commands/describe'),
+  options: [{
+    name: 'operation',
+    alias: 'o',
+    description: "The operation to describe",
   }]
 }, {
   name: 'authenticate <integration>',
+  examples: ["datafire authenticate github"],
+  description: "Store a set of credentials for a given integration",
   runner: require('./commands/authenticate'),
 }, {
-  name: 'run <flow>',
-  runner: require('./commands/run'),
-}, {
   name: 'call <integration>',
+  description: "Make a test call to an operation",
+  examples: ["datafire call hacker_news -o getUser -p.username sama"],
   runner: require('./commands/call'),
+  options: [{
+    name: 'operation',
+    alias: 'o',
+    description: "The operation to call",
+  }, {
+    name: 'as',
+    description: "The account alias to use",
+  }, {
+    name: 'params',
+    alias: 'p',
+    description: "Pass parameters to the operation",
+  }]
 }, {
-  name: 'describe <integration>',
-  runner: require('./commands/describe'),
+  name: 'run <flow>',
+  examples: ["datafire run ./flow.js"],
+  description: "Run a flow locally",
+  runner: require('./commands/run'),
 }]
 
-
-let args = require('yargs');
+let args = require('yargs')
+           .option('v', {alias: 'verbose'})
+           .global('v')
+           .recommendCommands();
 
 COMMANDS.forEach(cmd => {
   args = args.command(
@@ -58,12 +93,15 @@ COMMANDS.forEach(cmd => {
           } catch (e) {
             logger.logError(e.toString());
             if (args.verbose) {
-              throw e;
+              logger.log(e.stack);
             } else {
               process.exit(1);
             }
           }
         });
+  (cmd.examples || []).forEach(ex => {
+    args = args.example(cmd.name, ex);
+  })
 })
 
 args = args.help('h').alias('h', 'help').argv;
