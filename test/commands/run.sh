42d35522db78918b13f5956d91dd5550e38871f5
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-10 20:57:55 +0000

    fix golden file locations

--- test/commands/run.sh
+++ test/commands/run.sh
@@ -1,15 +1,15 @@
 set -e
-datafire list -a > ./list_all.txt
-datafire list    > ./list.txt
+datafire list -a > ./test/commands/list_all.txt
+datafire list    > ./test/commands/list.txt
 
-datafire integrate hacker_news > ./integrate.txt
-datafire describe hacker_news  > ./describe_hn.txt
-datafire describe hacker_news -o getItem > ./describe_getItem.txt
-datafire describe hacker_news -o "GET /item/{itemID}.json" > ./describe_getItem2.txt
+datafire integrate hacker_news > ./test/commands/integrate.txt
+datafire describe hacker_news  > ./test/commands/describe_hn.txt
+datafire describe hacker_news -o getItem > ./test/commands/describe_getItem.txt
+datafire describe hacker_news -o "GET /item/{itemID}.json" > ./test/commands/describe_getItem2.txt
 
-datafire call hacker_news -o getItem --params.itemID 444 > ./call_getItem.txt
-datafire call hacker_news -o "GET /item/{itemID}.json" --params.itemID 444 > ./call_getItem2.txt
+datafire call hacker_news -o getItem --params.itemID 444 > ./test/commands/call_getItem.txt
+datafire call hacker_news -o "GET /item/{itemID}.json" --params.itemID 444 > ./test/commands/call_getItem2.txt
 
 cd examples/quickstart
-datafire integrate hacker_news
-datafire run getTopStory.js > ./run.txt
+datafire integrate hacker_news > /dev/null
+datafire run getTopStory.js > ../../test/commands/run.txt
