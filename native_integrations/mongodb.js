1ba759e72abb9220ba6e77bee03e8e58515ca026
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-10 18:03:59 +0000

    fix refactor mongo spec

--- native_integrations/mongodb.js
+++ native_integrations/mongodb.js
@@ -1,39 +1,25 @@
 let datafire = require('../index');
 let mongodb = require('mongodb');
 
-let spec = {
-  "info": {
-    "title": "MongoDB",
-    "description": "Access to MongoDB"
+let SPEC = {
+  info: {
+    title: "MongoDB",
+    description: "Access to MongoDB"
   },
-  "securityDefinitions": {
-    "mongo_url": {
-      "type": "string",
-      "name": "url"
+  securityDefinitions: {
+    mongo_url: {
+      type: "string",
+      name: "url"
     }
   },
-  "operations": {
-    "insert": {}
-  }
-}
-
-class MongoDBOperation extends datafire.Operation {
-  constructor(name, integration, run) {
-    super(name, integration);
-    this.runQuery = run;
-    this.info.parameters = [{
-      name: 'collection',
-      type: 'string',
-      description: 'The MongoDB collection to operate on',
-      required: true,
-    }];
-    if (this.name === 'insert') {
-      this.info.parameters.push({
+  operations: {
+    insert: {
+      parameters: [{
         name: 'document',
         type: 'object',
         description: 'The document to insert',
-      })
-      this.info.response = {
+      }],
+      response: {
         schema: {
           properties: {
             ok: {type: 'integer'},
@@ -42,6 +28,20 @@ class MongoDBOperation extends datafire.Operation {
         }
       }
     }
+  }
+}
+
+class MongoDBOperation extends datafire.Operation {
+  constructor(name, integration, run) {
+    super(name, integration);
+    this.runQuery = run;
+    this.info = SPEC.operations[name];
+    this.info.parameters.unshift({
+      name: 'collection',
+      type: 'string',
+      description: 'The MongoDB collection to operate on',
+      required: true,
+    });
   }
 
   call(args, callback) {
@@ -56,7 +56,7 @@ class MongoDBOperation extends datafire.Operation {
 
 class MongoDBIntegration extends datafire.Integration {
   constructor(mockClient) {
-    super('mongodb', spec);
+    super('mongodb', SPEC);
     this.client = mockClient || mongodb.MongoClient;
     this.addOperation(new MongoDBOperation('insert', this, (collection, args, cb) => {
       collection.insert(args.document || args.documents, cb);
