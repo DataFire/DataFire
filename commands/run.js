a1a0bc701df4b7b9a292bac112a2b8de6eca6d20
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-11 03:12:07 +0000

    add setOptions

--- commands/run.js
+++ commands/run.js
@@ -1,9 +1,7 @@
 module.exports = function(args) {
   let flow = require(process.cwd() + '/' + args.flow);
   if (args.options) {
-    for (let key in args.options) {
-      flow.options[key] = args.options[key];
-    }
+    flow.setOptions(args.options);
   }
   flow.execute((err) => {
     if (err) throw err;
