a31cae8222370e4e31d88f05079013f5d42259f7
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-11 17:34:51 -0500

    update golden files

--- test/commands/run.sh
+++ test/commands/run.sh
@@ -1,24 +1,33 @@
 set -e
+echo "List..."
 datafire list -a > ./test/commands/list_all.txt
 datafire list    > ./test/commands/list.txt
 
+echo "Integrate..."
 datafire integrate hacker_news > ./test/commands/integrate.txt
+
+echo "Describe..."
 datafire describe hacker_news  > ./test/commands/describe_hn.txt
 datafire describe hacker_news -o getItem > ./test/commands/describe_getItem.txt
 datafire describe hacker_news -o "GET /item/{itemID}.json" > ./test/commands/describe_getItem2.txt
 
+echo "Call..."
 datafire call hacker_news -o getItem --params.itemID 444 > ./test/commands/call_getItem.txt
 datafire call hacker_news -o "GET /item/{itemID}.json" --params.itemID 444 > ./test/commands/call_getItem2.txt
 
+echo "Running quickstart..."
 cd examples/quickstart
 datafire integrate hacker_news > /dev/null
 datafire run getTopStory.js > ../../test/commands/quickstart.txt
 
+echo "Running authentication..."
 cd ../authentication
 datafire integrate github > /dev/null
 datafire run getUser.js > ../../test/commands/authentication.txt
 
+echo "Running error_handling..."
 cd ../error_handling
 datafire integrate hacker_news > /dev/null
 datafire run triggerError.js > ../../test/commands/error_handling.txt
 
+echo "Success"
