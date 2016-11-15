57c2a0f20bdd247b2f8e848b6d82c4205195c6db
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-11-15 11:57:39 +0000

    add patch for googleapis

--- commands/integrate.js
+++ commands/integrate.js
@@ -161,5 +161,19 @@ const maybePatchIntegration = (spec) => {
         })
       }
     }
+  } else if (spec.host === 'www.googleapis.com' && spec.securityDefinitions) {
+    let curDef = Object.keys(spec.securityDefinitions)
+          .map(k => spec.securityDefinitions[k])
+          .filter(d => d.type === 'oauth2')[0];
+    if (!curDef) return;
+    spec.securityDefinitions.offline = {
+      type: 'oauth2',
+      flow: 'accessCode',
+      scopes: curDef.scopes,
+      authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
+      tokenUrl: 'https://www.googleapis.com/oauth2/v4/token',
+      description: "Allows offline access using a refresh_token",
+    }
+
   }
 }
