6077084521e7417c689997396594ff52e0d41a2b
Bobby Brennan <bobby.brennan@gmail.com>
2016-11-18 18:49:28 +0100

    remove distro folder

--- distribution/entry.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.version > 'v6') {
-  module.exports = require('./index');
-} else {
-  module.exports = require('./distribution/index');
-}
--- distribution/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var datafire = module.exports = {};
-datafire.credentialsDirectory = process.cwd() + '/credentials';
-datafire.integrationsDirectory = process.cwd() + '/integrations';
-
-datafire.Integration = require('./lib/integration');
-datafire.Operation = require('./lib/operation');
-datafire.Flow = require('./lib/flow');
