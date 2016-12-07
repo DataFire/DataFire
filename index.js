59040a86b823df8fb802f16e3c5fa878ea8ae7d1
Ubuntu <ubuntu@ip-10-0-0-198.us-west-2.compute.internal>
2016-12-07 19:31:10 +0000

    add version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let datafire = module.exports = {};
-
+datafire.version = require('./package').version;
 datafire.commands = require('./commands');
 datafire.RESTIntegration = require('./lib/rest-integration');
 datafire.Integration = require('./lib/integration');
