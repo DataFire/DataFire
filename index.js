938d275be3bdda21f0fd878de47adf39be98153d
Bobby Brennan <bobby.brennan@gmail.com>
2016-12-07 15:05:54 -0500

    remove version

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let datafire = module.exports = {};
-datafire.version = require('./package').version;
 datafire.commands = require('./commands');
 datafire.RESTIntegration = require('./lib/rest-integration');
 datafire.Integration = require('./lib/integration');
