483e23678a33ecbc8d6a07e74aba5cc487deb86d
Travis CI <bobby.brennan@gmail.com>
2017-04-21 21:21:20 +0000

    use inputSchema to extend parameters array, add some tests

--- util/openapi.js
+++ util/openapi.js
@@ -1,5 +1,14 @@
 let openapi = module.exports = {};
 
+openapi.PATH_PARAM_REGEX = /\{([^\}]+)\}/g;
+openapi.PARAM_SCHEMA_FIELDS = [
+  'format', 'description', 'pattern', 'enum',
+  'maximum', 'minimum', 'exclusiveMaximum', 'exclusiveMinimum',
+  'maxLength', 'minLength',
+  'maxItems', 'minItems', 'uniqueItems',
+  'multipleOf',
+]
+
 openapi.initialize = function(spec, derefDefs=true) {
   if (derefDefs) {
     spec = dereference(spec, spec);
@@ -46,6 +55,85 @@ function dereference(obj, base, cache={}) {
     }
   }
   return obj;
+}
+
+openapi.getOperation = (method, path, pathOp) => {
+  let op = {
+    parameters: pathOp.parameters || [],
+    responses: pathOp.responses,
+    operationId: pathOp.operationId || pathOp.action.title,
+    description: pathOp.description || pathOp.action.description,
+  }
+  if (!op.operationId) delete op.operationId;
+  if (!op.description) delete op.description;
+  function maybeAddParam(param) {
+    let existing = op.parameters.filter(p => p.name === param.name && p.in === param.in)[0];
+    if (existing) return;
+    op.parameters.push(param);
+  }
+  let pathParams = path.match(openapi.PATH_PARAM_REGEX) || [];
+  pathParams.map(p => ({
+    in: 'path',
+    required: true,
+    name: p.substring(1, p.length - 1),
+    type: 'string',
+  })).forEach(maybeAddParam);
+
+  let needsBody = method === 'post' || method === 'patch' || method === 'put';
+  let hasBody = !!op.parameters.filter(p => p.in === 'formData' || p.in === 'body').length;
+  if (needsBody && !hasBody) {
+    op.parameters.push({
+      in: 'body',
+      name: 'body',
+      schema: pathOp.action.inputSchema,
+    })
+  }
+  let requiredProps = pathOp.action.inputSchema.required || [];
+  Object.keys(pathOp.action.inputSchema.properties || {}).forEach(prop => {
+    let param = op.parameters.filter(p => p.name === prop)[0];
+    if (!param && !needsBody) {
+      param = {in: 'query', name: prop};
+      op.parameters.push(param);
+    }
+    if (param) {
+      let schema = pathOp.action.inputSchema.properties[prop];
+      param.type = schema.type;
+      openapi.PARAM_SCHEMA_FIELDS
+          .filter(f => param[f] === undefined)
+          .filter(f => schema[f] !== undefined)
+          .forEach(f => param[f] = schema[f]);
+      if (requiredProps.indexOf(param.name) !== -1) param.required = true;
+    }
+  });
+  op.parameters.forEach(p => {
+    if (p.in === 'body') op.consumes = ['application/json'];
+    else if (p.in === 'formData') op.consumes = ['application/x-www-form-urlencoded'];
+  })
+  if (!op.responses) {
+    op.responses = {
+      200: {
+        description: 'Success',
+        schema: pathOp.action.outputSchema,
+      },
+      400: {
+        description: 'Invalid request',
+        schema: {
+          properties: {
+            error: {type: 'string'},
+          }
+        }
+      },
+      500: {
+        description: 'Unknown error',
+        schema: {
+          properties: {
+            error: {type: 'string'},
+          }
+        }
+      }
+    }
+  }
+  return op;
 }
 
 
