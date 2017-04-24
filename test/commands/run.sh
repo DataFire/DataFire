44b205552cc278d5830593caa8608559c0f91ac4
Travis CI <bobby.brennan@gmail.com>
2017-04-24 19:08:27 +0000

    use local cli for tests

--- test/commands/run.sh
+++ test/commands/run.sh
@@ -1,22 +1,22 @@
 set -e
 echo "Version..."
-datafire version > ./test/commands/version.txt
+./bin/datafire version > ./test/commands/version.txt
 
 echo "List..."
-datafire list -a > ./test/commands/list_all.txt
-datafire list    > ./test/commands/list.txt
+./bin/datafire list -a > ./test/commands/list_all.txt
+./bin/datafire list    > ./test/commands/list.txt
 
 echo "Integrate..."
 DATAFIRE_LOCATION="../../" \
-  datafire integrate --name hacker-news \
+  ./bin/datafire integrate --name hacker-news \
   --openapi "https://raw.githubusercontent.com/DataFire/Integrations/master/integrations/hacker-news/integration.json" \
   > ./test/commands/integrate.txt
 
 echo "Describe..."
-datafire describe hacker-news  > ./test/commands/describe_hn.txt
-datafire describe hacker-news/getItem > ./test/commands/describe_getItem.txt
+./bin/datafire describe hacker-news  > ./test/commands/describe_hn.txt
+./bin/datafire describe hacker-news/getItem > ./test/commands/describe_getItem.txt
 
 echo "Call..."
-datafire run hacker-news/getItem --input.itemID 444 > ./test/commands/call_getItem.txt
+./bin/datafire run hacker-news/getItem --input.itemID 444 > ./test/commands/call_getItem.txt
 
 echo "Success"
