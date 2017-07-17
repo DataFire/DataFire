c47100218ffe211d2b88ede33c8a472d1a839fe2
Travis CI <bobby.brennan@gmail.com>
2017-07-17 15:46:39 +0000

    add encoding option to datafire.Response

--- src/lib/response.js
+++ src/lib/response.js
@@ -3,6 +3,7 @@
 let Response = module.exports = function(opts) {
   this.statusCode = opts.statusCode || 200;
   this.body = opts.body || '';
+  this.encoding = opts.encoding || 'utf8';
   this.headers = opts.headers || {};
   if (opts.json !== undefined) {
     this.headers['Content-Type'] = 'application/json';
@@ -15,6 +16,5 @@ Response.prototype.send = function(res) {
   for (let header in this.headers) {
     res.set(header, this.headers[header]);
   }
-  res.send(this.body);
-  res.end();
+  res.end(this.body, this.encoding);
 }
